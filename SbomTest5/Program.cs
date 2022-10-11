1d14ea318b8a9ddddb1449a1d2eaaa4be99c7fa8
Kao, Bai Chien <bai.chien.kao@accenture.com>
2022-10-11 15:23:06 -0400

    updated 10112022

--- SbomTest5/Program.cs
+++ SbomTest5/Program.cs
@@ -1,8 +1,28 @@
 
+using System.IO;
+using ICSharpCode.SharpZipLib.Zip;
+using ICSharpCode.SharpZipLib.Zip.Compression;
+
+
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
+Object? A = null;
+Object? B = null;
+bool? C = null;
 
-app.MapGet("/", () => "Hello World!");
+C = OrchardCore.Contents.AdminMenu.Equals(A, B);
+C = Microsoft.AspNetCore.SpaServices.Prerendering.RenderToStringResult.Equals(A, B);
+C = Composite.Core.Application.ApplicationStartupAttribute.Equals(A, B);
+
+using (ZipOutputStream OutputStream = new ZipOutputStream(File.Create("Zip.zip")))
+{
+    OutputStream.SetLevel(9);
+    OutputStream.Finish();
+    OutputStream.Close();
+}
+
+
+    app.MapGet("/", () => "Hello World!");
 
 app.Run();
